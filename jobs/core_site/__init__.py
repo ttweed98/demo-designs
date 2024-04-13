9b8a2a603b9d88a2c39a1def39109cf681f92367
reshifirdous <141311017+reshifirdous@users.noreply.github.com>
2024-04-13 10:15:08 +0530

    Update __init__.py

--- jobs/core_site/__init__.py
+++ jobs/core_site/__init__.py
@@ -1,8 +1,40 @@
+# """Design to create a core backbone site."""
+# from nautobot.apps.jobs import register_jobs
+
+# from nautobot.dcim.models import Location
+
+# from nautobot.extras.jobs import ObjectVar, StringVar, IPNetworkVar
+
+# from nautobot_design_builder.design_job import DesignJob
+
+# from .context import CoreSiteContext
+
+
+# class CoreSiteDesign(DesignJob):
+#     """Create a core backbone site."""
+
+#     region = ObjectVar(
+#         label="Region",
+#         description="Region for the new backbone site",
+#         model=Location,
+#     )
+
+#     site_name = StringVar(regex=r"\w{3}\d+")
+
+#     site_prefix = IPNetworkVar(min_prefix_length=16, max_prefix_length=22)
+#     has_sensitive_variables = False
+
+#     class Meta:
+#         """Metadata needed to implement the backbone site design."""
+
+#         name = "Backbone Site Design"
+#         commit_default = False
+#         design_file = "designs/0001_design.yaml.j2"
+#         context_class = CoreSiteContext
+#         nautobot_version = ">=2"
+
 """Design to create a core backbone site."""
-from nautobot.apps.jobs import register_jobs
-
-from nautobot.dcim.models import Location
-
+from nautobot.dcim.models import Region
 from nautobot.extras.jobs import ObjectVar, StringVar, IPNetworkVar
 
 from nautobot_design_builder.design_job import DesignJob
@@ -16,13 +48,12 @@ class CoreSiteDesign(DesignJob):
     region = ObjectVar(
         label="Region",
         description="Region for the new backbone site",
-        model=Location,
+        model=Region,
     )
 
     site_name = StringVar(regex=r"\w{3}\d+")
 
     site_prefix = IPNetworkVar(min_prefix_length=16, max_prefix_length=22)
-    has_sensitive_variables = False
 
     class Meta:
         """Metadata needed to implement the backbone site design."""
@@ -31,7 +62,6 @@ class CoreSiteDesign(DesignJob):
         commit_default = False
         design_file = "designs/0001_design.yaml.j2"
         context_class = CoreSiteContext
-        nautobot_version = ">=2"
 
-name = "Demo Designs"
-register_jobs(CoreSiteDesign)
+# name = "Demo Designs"
+# register_jobs(CoreSiteDesign)
