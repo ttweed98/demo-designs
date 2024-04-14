593946d940c8753c373ba8d257921cee790e72b0
reshifirdous <141311017+reshifirdous@users.noreply.github.com>
2024-04-15 00:40:52 +0530

    df

--- jobs/sdwan_site/__init__.py
+++ jobs/sdwan_site/__init__.py
@@ -30,5 +30,5 @@ class CoreSiteDesign(DesignJob):
     def run(self, **kwargs):  # pylint: disable=arguments-differ
         """Render the design and implement it within a build Environment object."""
         # Set the design file based on user input
-        self.Meta.design_file = self.data.get('design_file')
+        self.Meta.design_file = kwargs.get('data', {}).get('design_file')
         super().run(**kwargs)
